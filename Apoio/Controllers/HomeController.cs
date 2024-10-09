45ac3318697f8c91ed7ae866274b8a8caa347157
3174 <3174@LAB1-PROF>
2024-10-08 21:03:02 -0300

    Adicionar arquivos de projeto.

---
+++ Apoio/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+using Apoio.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Apoio.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MyMethod()
+        {
+            ViewData["Nome"] = "Seu Nome";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Apoio/Controllers/TesteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Apoio.Controllers
+{
+    public class TesteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
