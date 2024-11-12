bba6d2bb464d70f779897c481d878a952d61f81d
rosenclever <rosenclever@gmail.com>
2024-11-12 19:10:00 -0300

    view aposta icompleta

--- Apoio/Controllers/ApostaController.cs
+++ Apoio/Controllers/ApostaController.cs
@@ -1,12 +1,64 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Apoio.Data;
+using Apoio.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
 
 namespace Apoio.Controllers
 {
     public class ApostaController : Controller
     {
+        private readonly WebetContext _context;
+
+        public ApostaController(WebetContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            var apostaContext = _context.Apostas.Include(ap => ap.Apostador);
+            return View(apostaContext.ToList());
+        }
+        public IActionResult Create()
+        {
+            ViewData["ApostadorId"] = new SelectList(_context.Apostadores, "Id", "Nome");
             return View();
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("Valor", "PrevisaoDeGanho", "Vencedora", "ApostadorId")] Aposta aposta)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewData["ApostadorId"] = new SelectList(_context.Apostadores, "Id", "Nome", aposta.Id);
+                return View(aposta);
+            }
+            if(aposta == null)
+            {
+                return NotFound();
+            }
+
+            _context.Add(aposta);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var aposta = await _context.Apostas.FindAsync(id);
+            if (aposta == null)
+            {
+                return NotFound();
+            }
+            return View(aposta);
+        }
+
     }
 }
